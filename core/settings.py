b6d1a963fe295b44da894b940f1e7376a9e0b704
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2023-04-24 20:09:33 -0700

    debugging res issue

--- core/settings.py
+++ core/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@m*)zgvo8w2mv*h=k^pd+66=)vi+frkj4suh(&+t=cnuowk6(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-2cb2.up.railway.app']
+ALLOWED_HOSTS = ['web-production-2cb2.up.railway.app', '127.0.0.1']
 
 
 # Application definition
