0a3e79289c47f0f319062824f5e913e22c3fb535
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2025-01-08 14:05:32 -0800

    new year, new update. fixed crash

--- core/__init__.py
+++ core/__init__.py
@@ -3,33 +3,12 @@ import subprocess
 import os
 
 
-HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
-
-if HF_AUTH_TOKEN:
-    print("Hugging Face token found.")
-else:
-    print("Hugging Face token NOT found!")
+HF_AUTH_TOKEN = os.environ.get('HF_AUTH_TOKEN')
 
 global emotion_model
 emotion_model = pipeline('sentiment-analysis', 
                         model='arpanghoshal/EmoRoBERTa', use_auth_token=HF_AUTH_TOKEN)
 
-
-
-# from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
-# import subprocess
-# import os
-
-# # HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
-# # print(HF_AUTH_TOKEN)
-
-# global emotion_model
-# tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
-# model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
-
-# emotion_model = pipeline('sentiment-analysis', 
-#                     model='arpanghoshal/EmoRoBERTa', use_auth_token='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'
-# )
 
 #will automatically download required corupus
 subprocess.run(["python", "-m", "textblob.download_corpora"])
