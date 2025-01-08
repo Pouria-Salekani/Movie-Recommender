844720cb20dcdd0e741c505514b37e21f7feed17
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2025-01-07 17:27:39 -0800

    cle

--- core/__init__.py
+++ core/__init__.py
@@ -17,13 +17,18 @@
 
 from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
 import subprocess
+import os
+
+# HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
+# print(HF_AUTH_TOKEN)
 
 global emotion_model
 tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
 model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 
 emotion_model = pipeline('sentiment-analysis', 
-                    model='arpanghoshal/EmoRoBERTa')
+                    model='arpanghoshal/EmoRoBERTa', use_auth_token='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'
+)
 
 #will automatically download required corupus
 subprocess.run(["python", "-m", "textblob.download_corpora"])
---
+++ core/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+import os
+
+HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
+print(HF_AUTH_TOKEN)
