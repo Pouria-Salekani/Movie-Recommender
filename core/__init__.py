bf6264891b46705bf5a97aa6e0e0f24b13f78874
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2025-01-07 17:21:56 -0800

    new no auth

--- core/__init__.py
+++ core/__init__.py
@@ -1,18 +1,29 @@
-from transformers import pipeline
+# from transformers import pipeline
+# import subprocess
+# import os
+
+# HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
+
+# if not HF_AUTH_TOKEN:
+#     print("HF_AUTH_TOKEN is not found in the environment!")
+# else:
+#     # Just show partial token to confirm we got something:
+#     print(f"HF_AUTH_TOKEN found, starts with: {HF_AUTH_TOKEN[:10]}...")
+# global emotion_model
+# emotion_model = pipeline('sentiment-analysis', 
+#                         model='arpanghoshal/EmoRoBERTa')
+
+
+
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
 import subprocess
-import os
 
-HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
-
-if not HF_AUTH_TOKEN:
-    print("HF_AUTH_TOKEN is not found in the environment!")
-else:
-    # Just show partial token to confirm we got something:
-    print(f"HF_AUTH_TOKEN found, starts with: {HF_AUTH_TOKEN[:10]}...")
 global emotion_model
-emotion_model = pipeline('sentiment-analysis', 
-                        model='arpanghoshal/EmoRoBERTa')
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 
+emotion_model = pipeline('sentiment-analysis', 
+                    model='arpanghoshal/EmoRoBERTa')
 
 #will automatically download required corupus
 subprocess.run(["python", "-m", "textblob.download_corpora"])
