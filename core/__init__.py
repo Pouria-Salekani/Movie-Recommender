48630ad2cffe2288625816ffc7393dae0420f7fe
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2025-01-07 16:43:59 -0800

    added new data

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,16 @@
 from transformers import pipeline
 import subprocess
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()  # reads .env file
+
+HF_AUTH_TOKEN = env('HF_AUTH_TOKEN', default=None)
+
 global emotion_model
 emotion_model = pipeline('sentiment-analysis', 
-                        model='arpanghoshal/EmoRoBERTa', use_auth_token=True)
+                        model='arpanghoshal/EmoRoBERTa', use_auth_token=HF_AUTH_TOKEN)
 
 
 #will automatically download required corupus
