3303e671b26dafb14d49a9b5cd1d52cc21418aab
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2025-01-07 18:53:45 -0800

    updated tok

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,18 @@
 from transformers import pipeline
 import subprocess
+import os
+
+
+HF_AUTH_TOKEN = os.environ.get("HF_AUTH_TOKEN")
+
+if HF_AUTH_TOKEN:
+    print("Hugging Face token found.")
+else:
+    print("Hugging Face token NOT found!")
 
 global emotion_model
 emotion_model = pipeline('sentiment-analysis', 
-                        model='arpanghoshal/EmoRoBERTa', use_auth_token='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')
+                        model='arpanghoshal/EmoRoBERTa', use_auth_token=HF_AUTH_TOKEN)
 
 
 
